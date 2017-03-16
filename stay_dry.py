7e00b97123dfe1acf3c31fd71592758c58a6b732
Chris de Graaf <chrisadegraaf@gmail.com>
2017-03-15 23:26:27 -0500

    Fix importing

--- stay_dry.py
+++ stay_dry.py
@@ -105,58 +105,59 @@ def write(fn, msg):
 
 # --------------- Actual script starts here --------------- #
 
-settings = read_config()
-# Sleep until 03:00 for the first run.
-start = datetime.now().replace(hour=3, minute=0, microsecond=0)
-if datetime.now().hour >= 3:
-    start += timedelta(days=1)
-delta = start - datetime.now()
-write(join(DATA_DIR, 'logs', 'start.log'), 'Sleeping for %s.' % delta)
-sleep(delta.total_seconds())
+if __name__ == '__main__':
+    settings = read_config()
+    # Sleep until 03:00 for the first run.
+    start = datetime.now().replace(hour=3, minute=0, microsecond=0)
+    if datetime.now().hour >= 3:
+        start += timedelta(days=1)
+        delta = start - datetime.now()
+        write(join(DATA_DIR, 'logs', 'start.log'), 'Sleeping for %s.' % delta)
+        sleep(delta.total_seconds())
 
-# Ideally this would run once per day rather than continuously,
-# but cron is not cooperating.
-while True:
-    # This line should be reached at 03:00 every day.
-    log = join(DATA_DIR, 'logs', '%s.log' % datetime.now().strftime('%m-%d-%Y'))  # noqa
+    # Ideally this would run once per day rather than continuously,
+    # but cron is not cooperating.
+    while True:
+        # This line should be reached at 03:00 every day.
+        log = join(DATA_DIR, 'logs', '%s.log' % datetime.now().strftime('%m-%d-%Y'))  # noqa
 
-    weekday = datetime.now().weekday()
-    if weekday > 4:
-        # On weekends, sleep until the next day.
-        write(log, 'It\'s not a weekday.')
+        weekday = datetime.now().weekday()
+        if weekday > 4:
+            # On weekends, sleep until the next day.
+            write(log, 'It\'s not a weekday.')
+            log.close()
+            sleep(timedelta(days=1).total_seconds())
+            continue
+
+        # Refresh these every day so that the date is updated.
+        mwf = (
+            parse(settings['mwf_bike']),
+            parse(settings['mwf_bus'])
+        )
+        tr = (
+            parse(settings['tr_bike']),
+            parse(settings['tr_bus'])
+        )
+
+        day = mwf if weekday % 2 == 0 else tr
+        early = min(day)
+
+        # Wait until 5 minutes before the earliest alarm to check the weather.
+        delta = early - datetime.now() - timedelta(minutes=5)
+        write(log, 'Sleeping for %s before making prediction.' % delta)
+        sleep(delta.total_seconds())
+
+        delta = get_delta(*day)
+        write(log, 'Sleeping for %s.' % delta)
+        sleep(delta.total_seconds())
+
+        write(log, 'Time to wake up.')
+        ring()
+
+        delta = (datetime.now() + timedelta(days=1)).replace(
+            hour=3, minute=0, microsecond=0
+        ) - datetime.now()
+        write(log, 'Sleeping for %s.' % delta)
+        sleep(delta.total_seconds())
+
         log.close()
-        sleep(timedelta(days=1).total_seconds())
-        continue
-
-    # Refresh these every day so that the date is updated.
-    mwf = (
-        parse(settings['mwf_bike']),
-        parse(settings['mwf_bus'])
-    )
-    tr = (
-        parse(settings['tr_bike']),
-        parse(settings['tr_bus'])
-    )
-
-    day = mwf if weekday % 2 == 0 else tr
-    early = min(day)
-
-    # Wait until 5 minutes before the earliest alarm to check the weather.
-    delta = early - datetime.now() - timedelta(minutes=5)
-    write(log, 'Sleeping for %s before making prediction.' % delta)
-    sleep(delta.total_seconds())
-
-    delta = get_delta(*day)
-    write(log, 'Sleeping for %s.' % delta)
-    sleep(delta.total_seconds())
-
-    write(log, 'Time to wake up.')
-    ring()
-
-    delta = (datetime.now() + timedelta(days=1)).replace(
-        hour=3, minute=0, microsecond=0
-    ) - datetime.now()
-    write(log, 'Sleeping for %s.' % delta)
-    sleep(delta.total_seconds())
-
-    log.close()
